9456a8d15787cd1cc7eebcecf10e371f051577c5
VINÍCIUS SOARES FRANÇA <vinicius_s_franca@estudante.sesisenai.org.br>
2024-10-29 12:13:00 -0300

    API

--- mobile/app/screens/SignUp.jsx
+++ mobile/app/screens/SignUp.jsx
@@ -1,53 +1,103 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Pressable, StyleSheet } from 'react-native';
+import { View, ScrollView, Text, TextInput, Image, Pressable, Alert, StyleSheet } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 import { router } from 'expo-router';
 import axios from 'axios';
 
 const SignUp = () => {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
-  const [dateBirth, setDateBirth] = useState('');
-  const [password, setPassword] = useState('');
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [dateBirth, setDateBirth] = useState("");
+  const [description, setDescription] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+  const [profilePic, setProfilePic] = useState(null);
 
   const handleSignUp = async () => {
-    try {
-      await axios.post('http://localhost:3000/api/auth/signup', {
-        firstName,
-        lastName,
-        email,
-        dateBirth,
-        password,
+    if (!email || !name || !password || !username || !passwordConfirm) return;
+
+    if (passwordConfirm !== password) {
+      return Alert.alert("As senhas devem ser iguais");
+    }
+
+    let formData = new FormData();
+    formData.append('name', name);
+    formData.append('username', username);
+    formData.append('email', email);
+    formData.append('password', password);
+    formData.append('description', description);
+    formData.append('dateBirth', dateBirth);
+    if (profilePic) {
+      formData.append('profilePic', {
+        uri: profilePic.uri,
+        type: profilePic.mimeType,
+        name: profilePic.fileName,
       });
-      router.push("/screens/SignIn");
-    } catch (error) {
-      alert(error.response ? error.response.data.message : "Erro ao cadastrar.");
+    }
+
+    try {
+      await axios.post(
+        "http://localhost:8000/user/register",
+        formData,
+        { headers: { "Content-Type": "multipart/form-data" } }
+      );
+      Alert.alert("Usuário criado com sucesso.");
+      router.push("/screens/Payments");
+    }
+    catch (e) {
+      console.log(e);
+      Alert.alert("Um erro ocorreu! Tente novamente.");
+    }
+  }
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+    if (!result.canceled) {
+      setProfilePic(result.assets[0]);
     }
   };
 
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <Text style={styles.title}>Crie sua conta</Text>
-      <TextInput value={firstName} onChangeText={setFirstName} placeholder="Nome" style={styles.input} placeholderTextColor="#ccc" />
-      <TextInput value={lastName} onChangeText={setLastName} placeholder="Sobrenome" style={styles.input} placeholderTextColor="#ccc" />
+      <TextInput value={name} onChangeText={setName} placeholder="Nome completo" style={styles.input} placeholderTextColor="#ccc" />
+      <TextInput value={username} onChangeText={setUsername} placeholder="Nome de usuário" style={styles.input} placeholderTextColor="#ccc" />
       <TextInput value={email} onChangeText={setEmail} placeholder="Email" style={styles.input} placeholderTextColor="#ccc" />
       <TextInput value={dateBirth} onChangeText={setDateBirth} placeholder="Data de Nascimento" style={styles.input} placeholderTextColor="#ccc" />
+      <TextInput value={description} onChangeText={setDescription} placeholder="Descrição" style={styles.input} placeholderTextColor="#ccc" />
       <TextInput value={password} onChangeText={setPassword} placeholder="Senha" secureTextEntry style={styles.input} placeholderTextColor="#ccc" />
+      <TextInput value={passwordConfirm} onChangeText={setPasswordConfirm} placeholder="Confirme sua senha" secureTextEntry style={styles.input} placeholderTextColor="#ccc" />
+      {profilePic !== null ? (
+        <Pressable onPress={pickImage} style={styles.buttonImg}>
+          <Image source={{ uri: profilePic.uri }} style={styles.image} />
+        </Pressable>
+      )
+        : (
+          <Pressable onPress={pickImage} style={styles.buttonImg}>
+            <Text>Adicionar Foto</Text>
+          </Pressable>
+        )}
+
       <Pressable style={styles.press} onPress={handleSignUp}>
-        <Text style={styles.pressText}>Continuar</Text>
+        <Text style={styles.pressText}>Cadastrar</Text>
       </Pressable>
       <View style={styles.signin}>
         <Text style={styles.signinText}>Já tem uma conta? </Text>
         <Text style={styles.signinLink} onPress={() => router.push("/screens/SignIn")}>Login</Text>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     padding: 20,
-    justifyContent: 'center',
     flex: 1,
     backgroundColor: '#1A1A1A',
   },
@@ -56,6 +106,10 @@ const styles = StyleSheet.create({
     color: '#FF6A00',
     marginBottom: 20,
     textAlign: 'center',
+  },
+  buttonImg: {
+    alignItems: 'center',
+    marginVertical: 10
   },
   input: {
     borderWidth: 1,
@@ -84,6 +138,11 @@ const styles = StyleSheet.create({
   },
   signinText: {
     color: '#fff',
+  },
+  image: {
+    width: 200,
+    height: 200,
+    borderRadius: 100
   },
   signinLink: {
     color: '#FF6A00',
