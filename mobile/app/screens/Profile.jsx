bcc30100ad7c9ec5093b399efbf57d50fc911831
Vini <viniciusicmsc@gmail.com>
2024-11-11 10:54:37 -0300

    sonar

--- mobile/app/screens/Profile.jsx
+++ mobile/app/screens/Profile.jsx
@@ -1,52 +1,97 @@
-import React, { useContext } from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
-import { AppContext } from '../context/AppContext'; 
+import React, { useState, useEffect, useContext } from "react";
+import { View, StyleSheet, Text, Image, Pressable } from "react-native";
+import axios from 'axios';
+import { AppContext } from "../scripts/AppContext";
 
-const Profile = () => {
-  const { user } = useContext(AppContext);
+export default function Profile() {
+  const { user, token } = useContext(AppContext);
+  const [data, setData] = useState({});
+
+  const getUserInfo = async () => {
+    try {
+      const res = await axios.get(
+        `http://10.0.2.2:8000/user/${user.id}`
+      )
+      res.data.data.createdAt = res.data.data.createdAt.split("T")[0];
+      setData(res.data.data);
+    }
+    catch (e) {
+      console.log(e);
+    }
+  }
+
+  useEffect(() => {
+    getUserInfo();
+  }, []);
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Perfil</Text>
-      {user.profileImage ? (
-        <Image source={{ uri: user.profileImage }} style={styles.image} />
-      ) : (
-        <Text style={styles.noImage}>Sem imagem de perfil</Text>
-      )}
-      <Text style={styles.text}>Nome: {user.name}</Text>
-      <Text style={styles.text}>Username: {user.username}</Text>
-      <Text style={styles.text}>Email: {user.email}</Text>
-      <Text style={styles.text}>Data de Nascimento: {user.dateBirth}</Text>
+      <View style={styles.info}>
+        {
+          data.profileImage !== null ? (
+            <Image source={{ uri: data.profileImage }} style={styles.image} />
+          ) : (
+            <Image source={require('../../assets/profile.png')} style={styles.image} />
+          )
+        }
+        <Text style={styles.username}>{data.username}</Text>
+        <Text style={styles.email}>{data.email}</Text>
+        <Text style={styles.data}>Member since {data.createdAt}</Text>
+        <Pressable style={styles.pressableEdit}>
+          <Text style={styles.pressableText}>Editar conta</Text>
+        </Pressable>
+      </View>
+
     </View>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
+  image: {
+    width: 150,
+    height: 150,
+    marginTop: 50,
+    marginBottom: 50,
+    borderRadius: 300,
+  },
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'space-between',
     alignItems: 'center',
-    backgroundColor: '#1A1A1A',
+    backgroundColor: "#000"
   },
-  title: {
-    fontSize: 24,
-    color: '#FFF',
-    marginBottom: 20,
+  info: {
+    width: '80%',
+    alignItems: 'center',
   },
-  image: {
-    width: 100,
-    height: 100,
-    borderRadius: 50,
-    marginBottom: 20,
+  username: {
+    color: 'white',
+    fontStyle: 'bold',
+    fontSize: 25,
+    marginBottom: 10
   },
-  noImage: {
-    color: '#FFF',
-    marginBottom: 20,
-  },
-  text: {
+  email: {
+    color: 'white',
+    fontStyle: 'bold',
     fontSize: 18,
-    color: '#FFF',
+    marginBottom: 10
   },
-});
-
-export default Profile;
+  data: {
+    color: 'white',
+    fontStyle: 'bold',
+    fontSize: 15,
+    marginBottom: 30
+  },
+  pressableEdit: {
+    width: 150,
+    height: 45,
+    borderRadius: 50,
+    backgroundColor: '#3D0075',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  pressableText: {
+    color: '#fff',
+    fontSize: 18
+  },
+})
